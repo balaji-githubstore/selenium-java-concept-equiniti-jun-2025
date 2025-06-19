358b6be116252854f3379b47ba694fdffe7088f5
balaji-githubstore <balajidinakaran1@gmail.com>
2025-06-19 12:48:26 +0530

    Alert Handing

--- src/com/eq/alertdemo/Demo1.java
+++ src/com/eq/alertdemo/Demo1.java
@@ -2,6 +2,7 @@ package com.eq.alertdemo;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,14 +11,23 @@ public class Demo1 {
 	public static void main(String[] args) {
 		
 		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
-		
 		driver.get("https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm");
 		
-	}
+		//click on Go
+		driver.findElement(By.xpath("//img[@alt='Go']")).click();
+		
+		//wait for alert to present
+		
+		String actualAlertText= driver.switchTo().alert().getText();
+		System.out.println(actualAlertText);
+		
+		driver.switchTo().alert().accept();
 
+		
+	}
 }
